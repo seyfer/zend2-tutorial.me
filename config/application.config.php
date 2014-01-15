d4d66d224a761ea382bb387cb41f690d1420b592
seyfer <seyferseed@mail.ru>
2014-01-15 14:10:09 +0800

    Disable cache conf for future fix

--- config/application.config.php
+++ config/application.config.php
@@ -18,7 +18,7 @@ if ($env == 'development') {
 }
 
 $stringKeyCache     = 'app_config';
-$stringKeyCachePath = 'data/config/';
+$stringKeyCachePath = 'data/cache/config/';
 $stringKeyModule    = 'module_map';
 
 
@@ -44,13 +44,13 @@ return array(
         // Whether or not to enable a configuration cache.
         // If enabled, the merged configuration will be cached and used in
         // subsequent requests.
-        'config_cache_enabled'     => ($env == 'production'),
+//        'config_cache_enabled'     => ($env == 'production'),
         // The key used to create the configuration cache file name.
         'config_cache_key'         => $stringKeyCache,
         // Whether or not to enable a module class map cache.
         // If enabled, creates a module class map cache which will be used
         // by in future requests, to reduce the autoloading process.
-        'module_map_cache_enabled' => ($env == 'production'),
+//        'module_map_cache_enabled' => ($env == 'production'),
         // The key used to create the class map cache file name.
         'module_map_cache_key'     => $stringKeyModule,
         // The path in which to cache merged configuration.
