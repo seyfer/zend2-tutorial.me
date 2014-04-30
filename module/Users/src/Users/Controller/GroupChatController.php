590abad05a69ce6cfb65296c919a196cab391a31
seyfer <seyferseed@mail.ru>
2014-04-30 17:27:51 +0800

    Complete group chat with no automated update.

--- module/Users/src/Users/Controller/GroupChatController.php
+++ module/Users/src/Users/Controller/GroupChatController.php
@@ -23,9 +23,9 @@ class GroupChatController extends BaseController
 
         $messageList = array();
         foreach ($chatMessages as $chatMessage) {
-            $fromUser            = $userTable->getUser($chatMessage->user_id);
+            $fromUser            = $userTable->getById($chatMessage->user_id);
             $messageData         = array();
-            $messageData['user'] = $fromUser->name;
+            $messageData['user'] = $fromUser->getName();
             $messageData['time'] = $chatMessage->stamp;
             $messageData['data'] = $chatMessage->message;
             $messageList[]       = $messageData;
@@ -34,6 +34,77 @@ class GroupChatController extends BaseController
         $viewModel = new ViewModel(array('messageList' => $messageList));
         $viewModel->setTemplate('users/group-chat/message-list');
         $viewModel->setTerminal(true);
+
+        return $viewModel;
+    }
+
+    protected function sendMessage($messageTest, $fromUserId)
+    {
+
+        $chatMessageTG = $this->getServiceLocator()
+                ->get('ChatMessagesTableGateway');
+        $data          = array(
+            'user_id' => $fromUserId,
+            'message' => $messageTest,
+            'stamp'   => NULL
+        );
+        $chatMessageTG->insert($data);
+        return true;
+    }
+
+    private function getLoggedInUser()
+    {
+        $authService = $this->getAuthService();
+        $email       = $authService->getStorage()->read();
+
+        $userTable = $this->getServiceLocator()->get('UserTable');
+        $user      = $userTable->getUserByEmail($email);
+
+        return $user;
+    }
+
+    public function indexAction()
+    {
+        $user    = $this->getLoggedInUser();
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $messageTest = $request->getPost()->get('message');
+            $fromUserId  = $user->getId();
+            $this->sendMessage($messageTest, $fromUserId);
+            // Для предотвращения дублирования записей при обновлении
+            return $this->redirect()->toRoute('group-chat');
+        }
+
+        // Подготовка формы отправки сообщения
+        $form = new \Zend\Form\Form();
+        $form->add(array(
+            'name'       => 'message',
+            'attributes' => array(
+                'type'     => 'text',
+                'id'       => 'messageText',
+                'required' => 'required'
+            ),
+            'options'    => array(
+                'label' => 'Message',
+            ),
+        ));
+        $form->add(array(
+            'name'       => 'submit',
+            'attributes' => array(
+                'type'  => 'submit',
+                'value' => 'Send'
+            ),
+        ));
+        $form->add(array(
+            'name'       => 'refresh',
+            'attributes' => array(
+                'type'  => 'button',
+                'id'    => 'btnRefresh',
+                'value' => 'Refresh'
+            ),
+        ));
+
+        $viewModel = new ViewModel(array('form' => $form, 'userName' => $user->getName()));
 
         return $viewModel;
     }
